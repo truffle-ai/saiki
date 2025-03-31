2623c3cca60083c2e45206ecd47fff951c577b30
Shaunak Srivastava <shaunak1999@gmail.com>
2025-03-31 16:59:11 -0700

    feat: Create ToolProvider interface to standardize tool providers

---
+++ src/client/types.ts
@@ -0,0 +1,28 @@
+import { McpTool } from '../ai/types.js';
+
+/**
+ * Interface for any client that can provide tools
+ */
+export interface ToolProvider {
+  /**
+   * Get the list of tools provided by this client
+   */
+  getTools(): Promise<McpTool[]>;
+  
+  /**
+   * Call a specific tool with the given arguments
+   */
+  callTool(toolName: string, args: any): Promise<any>;
+  
+  /**
+   * Disconnect the client (if applicable)
+   */
+  disconnect?(): Promise<void>;
+}
+
+/**
+ * A tool with its metadata
+ */
+export interface Tool extends McpTool {
+  // Extended as needed
+} 
