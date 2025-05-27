dab62b83746f5317c52dc06b57e18fcdc8ce3b10
Rahul Karajgikar <50844303+rahulkarajgikar@users.noreply.github.com>
2025-05-26 22:08:38 -0700

    Add docusaurus docs pages (#128)

---
+++ docs/docs/user-guide/development.md
@@ -0,0 +1,22 @@
+---
+sidebar_position: 4
+---
+
+# Application Development Guide
+
+This document describes how to build AI powered applications using Saiki-core.
+
+**Set up your project**
+
+Setup a fresh typescript project using saiki-core
+```bash
+saiki create-app
+```
+
+Add saiki into an existing typescript project
+```bash
+saiki init-app
+```
+
+
+<!-- ## Examples -->
---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,141 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+    title: 'Saiki',
+    tagline: 'Build AI Agents with ease',
+    favicon: 'img/favicon.ico',
+
+    // Set the production url of your site here
+    url: 'https://truffle-ai.github.io',
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: '/saiki/',
+
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: 'truffle-ai', // Usually your GitHub org/user name.
+    projectName: 'saiki', // Usually your repo name.
+
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en'],
+    },
+
+    presets: [
+        [
+            'classic',
+            {
+                docs: {
+                    sidebarPath: './sidebars.ts',
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl: 'https://github.com/truffle-ai/saiki/tree/main/docs/',
+                },
+                blog: {
+                    showReadingTime: true,
+                    feedOptions: {
+                        type: ['rss', 'atom'],
+                        xslt: true,
+                    },
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl: 'https://github.com/truffle-ai/saiki/tree/main/docs/',
+                    // Useful options to enforce blogging best practices
+                    onInlineTags: 'warn',
+                    onInlineAuthors: 'warn',
+                    onUntruncatedBlogPosts: 'warn',
+                },
+                theme: {
+                    customCss: './src/css/custom.css',
+                },
+            } satisfies Preset.Options,
+        ],
+    ],
+
+    themeConfig: {
+        // Replace with your project's social card
+        image: 'img/docusaurus-social-card.jpg',
+        navbar: {
+            title: 'Saiki',
+            logo: {
+                alt: 'Saiki Logo',
+                src: 'img/favicon.ico',
+            },
+            items: [
+                {
+                    type: 'docSidebar',
+                    sidebarId: 'tutorialSidebar',
+                    position: 'left',
+                    label: 'Docs',
+                },
+                { to: '/blog', label: 'Blog', position: 'left' },
+                {
+                    href: 'https://github.com/truffle-ai/saiki',
+                    label: 'GitHub',
+                    position: 'right',
+                },
+            ],
+        },
+        footer: {
+            style: 'dark',
+            links: [
+                {
+                    title: 'Docs',
+                    items: [
+                        {
+                            label: 'Tutorial',
+                            to: '/docs/getting-started/intro',
+                        },
+                    ],
+                },
+                {
+                    title: 'Community',
+                    items: [
+                        {
+                            label: 'GitHub Issues',
+                            href: 'https://github.com/truffle-ai/saiki/issues',
+                        },
+                        {
+                            label: 'Discord',
+                            href: 'https://discord.gg/GFzWFAAZcm',
+                        },
+                        {
+                            label: 'X',
+                            href: 'https://x.com/truffleai_',
+                        },
+                    ],
+                },
+                {
+                    title: 'More',
+                    items: [
+                        {
+                            label: 'Blog',
+                            to: '/blog',
+                        },
+                        {
+                            label: 'GitHub',
+                            href: 'https://github.com/truffle-ai/saiki',
+                        },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} Truffle AI.`,
+        },
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
+        },
+    } satisfies Preset.ThemeConfig,
+};
+
+export default config;
